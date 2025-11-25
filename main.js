0e4fe3b32ba4ae1663a407eba89826dfebf087da
Abdujabborov Abdurahmon <abdujabborovabdurahmon13@gmail.com>
2025-11-25 07:34:09 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+let summa = 100000
+let raqam = [9, 5, 6, 8, ]  
+let son = 4
+
+for (let r of raqam) {
+
+    if (summa >= 8000 && r == 1) {
+        console.log('Colanini olur');
+    }
+
+    else if (summa >= 12000 && r == 2) {
+        console.log('snikersni  olur');
+    }
+
+    else if (summa >= 8000 && r == 3) {
+        console.log('ice tea olur');
+    }
+
+    else if (summa >= 8000 && r == 4) {
+        console.log('fuseteani olur');
+    }
+
+    else if (summa >= 12000 && r == 5) {
+        console.log('strobar olur');
+    }
+
+    else if (summa >= 15000 && r == 6) {
+        console.log('cheersni olur');
+    }
+
+    else if (summa >= 15000 && r == 7) {
+        console.log('laysni olur');
+    }
+
+    else if (summa >= 15000 && r == 8) {
+        console.log('suvnini olur');
+    }
+
+    else if (summa >= 18000 && r == 9) {
+        console.log('garillanini olur');
+    }
+
+    else if (summa >= 8000 && r == 10) {
+        console.log('pepsini olur');
+    }
+
+    else {
+        console.log('Mablag yetarli emas yoki son raqam togri emas');
+    }
+}
